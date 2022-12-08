23e7604ed905ac049e1fe790a91058d53e2ebbed
JohnTNP <sy.tanaphon@gmail.com>
2022-12-08 17:04:51 +0700

    Remove Glow Title

--- src/pages/Home_Page.svelte
+++ src/pages/Home_Page.svelte
@@ -14,18 +14,7 @@
   .glow {
     user-select: none;
     font-size: 5rem;
-    color: #fff;
+    color: #0794a7;
     text-align: center;
-    animation: glow 3s ease-in-out infinite alternate;
-  }
-  @keyframes glow {
-    from {
-      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #7ba5bd,
-        0 0 40px #7ba5bd, 0 0 50px #7ba5bd, 0 0 60px #7ba5bd, 0 0 70px #7ba5bd;
-    }
-    to {
-      text-shadow: 0 0 20px #fff, 0 0 30px #0794a7, 0 0 40px #0794a7,
-        0 0 50px #0794a7, 0 0 60px #0794a7, 0 0 70px #0794a7, 0 0 80px #0794a7;
-    }
   }
 </style>
